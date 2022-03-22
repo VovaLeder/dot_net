e310a92aa230dfbd12c3688e8016ed661a0f2532
VovaLeder <bliznukvova2@gmail.com>
2022-03-22 20:00:22 +0300

    i did the event task

--- main.cs
+++ main.cs
@@ -1,10 +1,16 @@
-// Hello World! program
-namespace HelloWorld
+using dot_net_1;
+
+namespace dot_net_1
 {
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            
+            SomeClass someClass = new SomeClass();
+            someClass.OnKeyPressed += (sender, key) => System.Console.WriteLine("You wrote: " + key);
+
+            someClass.Run();
+
         }
     }
 }
---
+++ someClass.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace dot_net_1
+{
+    class SomeClass {
+        public event EventHandler<char> OnKeyPressed;
+
+        private char getChar(){
+            Console.Write("Your symbol: ");
+            char c;
+            try
+            {
+                c = Console.ReadLine().ToCharArray()[0];
+            }
+            catch (System.IndexOutOfRangeException e)
+            {
+                c = getChar();
+            }
+            
+            return c;
+        }
+        public void Run(){
+            char c = getChar();
+            while (c != 'c')
+            {
+                OnKeyPressed.Invoke(this, c);
+                c = getChar();
+            }
+        }
+    }
+}
