1e64b7059071f72f05b49c4ad2b9061718252b6b
VovaLeder <bliznukvova2@gmail.com>
2022-03-28 10:59:28 +0300

    close to finish

---
+++ Collections_Task/Entity.cs
@@ -0,0 +1,21 @@
+namespace Collections_Task
+{
+    class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+
+        public Entity(int id, int parentId, string name)
+        {
+            Id = id;
+            ParentId = parentId;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name + Id;
+        }
+    }
+}
--- Collections_Task/Program.cs
+++ Collections_Task/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Collections_Task;
+
+namespace Collections_Task
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        { 
+
+            Entity[] entities = new Entity[]
+            {
+                new Entity(1, 0, "Root entity"),
+                new Entity(2, 1, "Child of 1 entity"),
+                new Entity(3, 1, "Child of 1 entity"),
+                new Entity(4, 2, "Child of 2 entity"), 
+                new Entity(5, 4, "Child of 4 entity")
+            };
+
+            Dictionary<int, List<Entity>> dict = ArrayToDictionary(entities);
+
+            foreach (KeyValuePair<int, List<Entity>> keyvalue in dict)
+            {
+                Console.WriteLine($"{keyvalue.Key}: {keyvalue.Value.ToString()}");
+            }
+
+        }
+
+        static Dictionary<int, List<Entity>> ArrayToDictionary(Entity[] entities){
+            Dictionary<int, List<Entity>> dict = new Dictionary<int, List<Entity>>();
+
+            foreach(Entity entity in entities){
+                if (!dict.ContainsKey(entity.ParentId))
+                {
+                    dict.Add(entity.ParentId, new List<Entity>());
+                }
+                dict[entity.ParentId].Add(entity);
+            }
+
+            return dict;
+        }
+    }
+}
